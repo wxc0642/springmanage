d35aeac3d8e62f73f56b3700aa74586692bad896
huangjunjie <huangjunjie@saicmobility.com>
2021-01-05 19:32:12 +0800

    增加junit5测试

--- src/test/java/com/wei/SpringmanageApplicationTests.java
+++ src/test/java/com/wei/SpringmanageApplicationTests.java
@@ -1,22 +1,26 @@
 package com.wei;
 
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import javax.sql.DataSource;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringmanageApplicationTests {
 
-   @Autowired
-   DataSource dataSource;
-
     @Test
-    void contextLoads() {
+    void loginTest(@Autowired MockMvc mvc) throws Exception {
 
+        mvc.perform(MockMvcRequestBuilders
+                .post("/toLogin")
+                .param("username","root","password","123"))
+                .andExpect(status().isOk());
     }
 
 }
