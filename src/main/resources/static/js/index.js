5529781d613b5ea20162b3b063f5083029c58e3a
huangjunjie <1806620741@qq.com>
2021-01-10 14:39:49 +0800

    修复js资源路径生成问题

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,7 +10,7 @@
         // 配置所有应用的入口文件，程序将会按照data-main属性中设置的值进行索引查找
         // 如果你在引入此脚本的script标签上没有设置data-main属性，程序将会默认访问home.js文件
         app = {
-            home : '{/}home',
+            home : '{/}js/home',
             login : '{/}login'
         };
 
